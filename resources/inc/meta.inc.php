c2ab5d304ddb4baadb84e4b3f40baf41484bbfaf
freieslebenadam <adam.fre@seznam.cz>
2021-08-06 12:10:22 +0200

    basic structure

---
+++ resources/inc/meta.inc.php
@@ -0,0 +1,6 @@
+<!-- CHARSET -->
+<meta charset="UTF-8">
+<!-- EDGE -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<!-- VIEWPORT -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
