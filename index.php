99d448515256808a62b9d2a9098220d36a927fa6
freieslebenadam <adam.fre@seznam.cz>
2021-08-06 12:42:58 +0200

    autoloader

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
+<?php include 'resources/inc/autoloader.inc.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <?php include_once './resources/inc/meta.inc.php'; ?>
+    <?php require_once './resources/inc/meta.inc.php'; ?>
     <title>Document</title>
 </head>
+
 <body>
-    
+    <h1>Hello world</h1>
 </body>
+
 </html>
--- resources/inc/general.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// DATABASE
-require_once '../class/dabatase.class.php';
-// USERS MVC
-require_once '../class/users/users.model.class.php';
-require_once '../class/users/users.view.class.php';
-require_once '../class/users/users.controller.class.php';
